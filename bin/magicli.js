279b9559fe183e5d97ad3b81237f0600d9d027f2
Joel Lonbeck <joel@neutrinographics.com>
2018-07-10 15:09:14 -0700

    updated cli interface

--- bin/magicli.js
+++ bin/magicli.js
@@ -1,3 +1,51 @@
 #!/usr/bin/env node
 
-require('magicli')();
+require('magicli')({
+  validateRequiredParameters: true,
+  commands: {
+    alignFiles: {
+      description: 'Reads alignment data from a file and injects it into a USFM3 file.',
+      options: [
+        {
+          name: 'alignmentsFile',
+          alias: 'a',
+          description: 'Path to the wordMAP alignments file',
+          type: 'String',
+          required: true
+        },
+        {
+          name: 'usfmFile',
+          alias: 'u',
+          description: 'Path to the USFM3 file',
+          type: 'String',
+          required: true
+        },
+        {
+          name: 'outputFile',
+          alias: 'o',
+          description: 'Path to the output USFM3 file',
+          type: 'String',
+          required: true
+        }]
+    },
+    align: {
+      description: 'Injects raw alignment data into raw USFM data.',
+      options: [
+        {
+          name: 'alignments',
+          alias: 'a',
+          description: 'The alignment data as a JSON string',
+          type: 'String',
+          required: true
+        },
+        {
+          name: 'usfm',
+          alias: 'u',
+          description: 'The USFM3 data as a string',
+          type: 'String',
+          required: true
+        }
+      ]
+    }
+  }
+});
--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,25 @@ import {alignUSFM3} from './Injector';
 import * as fs from 'fs-extra';
 
 /**
- * Injects wordMAP alignments into a USFM file.
+ * Reads data from files and writes the aligned USFM to a file.
  * @param {string} alignmentsFile - path to the alignments file
  * @param {string} usfmFile - path to the usfm file
- * @param {string} output - path to the output usfm file
+ * @param {string} outputFile - path to the output usfm file
  */
-module.exports.alignFiles = (alignmentsFile: string, usfmFile: string, output: string) => {
+module.exports.alignFiles = (alignmentsFile: string, usfmFile: string, outputFile: string) => {
     const alignments = fs.readFileSync(alignmentsFile).toString();
     const usfm = fs.readFileSync(usfmFile).toString();
     const alignmentsJson = JSON.parse(alignments);
     const alignedUSFM = alignUSFM3(alignmentsJson, usfm);
-    fs.writeFileSync(output, alignedUSFM);
+    fs.writeFileSync(outputFile, alignedUSFM);
 };
 
-module.exports.align = alignUSFM3;
+/**
+ * Aligns USFM and returns the result
+ * @param {string} alignments
+ * @param {string} usfm
+ * @return {string}
+ */
+module.exports.align = (alignments: string, usfm: string) => {
+    return alignUSFM3(JSON.parse(alignments), usfm);
+};
